2b5be169bb65d18b32d3135087d1db0508dcce3a
nsfabina <nicholas.fabina@gmail.com>
2016-03-13 10:07:41 -0700

    Moves files from twitbank to tweetbank.

--- tweetbank/manage.py
+++ tweetbank/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitbank.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tweetbank.settings")
 
     from django.core.management import execute_from_command_line
 
