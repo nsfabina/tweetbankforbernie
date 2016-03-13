5874cc0edbf8240fcbe02ab02c3f7ffff741e176
nsfabina <nicholas.fabina@gmail.com>
2016-03-13 13:47:52 -0700

    Removes debug settings.

--- tweetbank/tweetbank/settings.py
+++ tweetbank/tweetbank/settings.py
@@ -13,24 +13,17 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = 'y#18=+gwzkjd-gczo9h%pkk_w6jmtv+%r@ss#%d3t_zt02e31)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -46,7 +39,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.twitter',
 ]
-
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -57,9 +49,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'tweetbank.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -77,13 +67,9 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'tweetbank.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -91,16 +77,12 @@ DATABASES = {
     }
 }
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/1.9/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 STATICFILES_DIRS = ['app/static']
@@ -108,7 +90,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/var/www/tweetbank/static/'
 
 # Django-allauth
-
 LOGIN_REDIRECT_URL = '/'
 SITE_ID = 1
 AUTHENTICATION_BACKENDS = (
