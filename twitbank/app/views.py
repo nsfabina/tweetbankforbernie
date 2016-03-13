095bf2b27f97a182c59a8a9858abf97edce1d91a
nsfabina <nicholas.fabina@gmail.com>
2016-03-12 16:15:18 -0800

    Switches from server to client oembed.

--- twitbank/app/views.py
+++ twitbank/app/views.py
@@ -22,11 +22,6 @@ _SANDERS_HASHTAGS = [
     ]
 _TWEET_LIMIT = 20
 
-# Global variables for oembed urls
-_OEMBED_URL = 'https://api.twitter.com/1/statuses/oembed.json?id={id}&' + \
-    'maxwidth=550&hide_media=true&hide_thread=true&align=center&omit_script=true'
-#get_oembed_tweet
-
 # Global variables for voting tweets
 _VOTE_TEXT = 'Remember to vote! Register on time! Dates and rules here:'
 _VOTE_URL = 'https://voteforbernie.org'
@@ -66,12 +61,10 @@ class HomeView(View):
         timeline = twitter.get_home_timeline(**_TIMELINE_ARGS)
         recent_tweets = _get_most_recent_sanders_tweets(username, timeline)
         tweet_context = []
-        for username, status_id in recent_tweets:
-            tweet_embed = json.loads(requests.get(_OEMBED_URL.format(
-                id=status_id)).text)['html']
-            tweet_vote = _TWEET_VOTE.format(username=username)
-            tweet_context.append([tweet_embed, tweet_vote])
-        context = {'user': username, 'tweet_context': tweet_context,
+        for tweeter, status_id in recent_tweets:
+            tweet_vote = _TWEET_VOTE.format(username=tweeter)
+            tweet_context.append([status_id, tweet_vote])
+        context = {'username': username, 'tweet_context': tweet_context,
                    'tweet_help': _TWEET_BANK, 'tweet_activism': _TWEET_ACTIVISM}
         return render(request, 'home.html', context=context)
 
