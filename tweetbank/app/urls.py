1f686a644a53ed75cdd688aff04a6248cbc16008
nsfabina <nicholas.fabina@gmail.com>
2016-03-16 09:43:38 -0700

    Removes 05/15 states from webapp.

--- tweetbank/app/urls.py
+++ tweetbank/app/urls.py
@@ -6,10 +6,5 @@ import app.views as app_views
 
 urlpatterns = [
     url(r'^(|/)$', app_views.HomeView.as_view()),
-    url(r'^florida$', app_views.StateView.as_view(state='FL')),
-    url(r'^illinois$', app_views.StateView.as_view(state='IL')),
-    url(r'^missouri$', app_views.StateView.as_view(state='MO')),
-    url(r'^ohio$', app_views.StateView.as_view(state='OH')),
-    url(r'^northcarolina$', app_views.StateView.as_view(state='NC')),
     url(r'^faqs$', TemplateView.as_view(template_name='faqs.html')),
     ]
